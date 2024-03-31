8778f17f8fa615c3d8f34494363c4e5eef451106
Grisham <doublegomosh00@gmail.com>
2024-04-01 01:58:16 +0300

    position-changes

--- src/components/Hero_section/Hero.jsx
+++ src/components/Hero_section/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Hero.css";
 import {HiLocationMarker} from 'react-icons/hi';;
 import CountUp from 'react-countup';
-
+import {motion}from 'framer-motion'
 const Hero = () => {
   return (
 <section className='hero-wrapper'>
@@ -14,11 +14,21 @@ const Hero = () => {
     <div className='flexColStart hero-left'>
          <div className='hero-title'>
             <div className='orange-circle'/>
-            <h1>
+
+        <motion.h1
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+                duration: 2,
+                type: "spring"
+            }}
+        >
+            
             Discover <br/> 
             The Best Properties <br/>
             For Happy Living
-            </h1>
+        
+            </motion.h1>
         </div>
 
         <div className='flexColStart hero-des'>
@@ -71,9 +81,17 @@ Find a variety of properties that suit you very easily
 
 {/* right side*/}
     <div className='flexCenter hero-right'>
-        <div className='image-container'>
+                <motion.div 
+                initial={{ x: "7rem", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{
+                    duration: 2,
+                    type: 'spring'
+                }}
+
+        className='image-container'>
         <img src='./hero-image.png' alt=''/>
-        </div>
+        </motion.div>
     </div>
 
 </div>
